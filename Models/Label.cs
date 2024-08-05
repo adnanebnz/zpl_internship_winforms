6b2f8e2963c98cd8f3898fa3f8657af3734a3dcf
unknown <skillzdev@hotmail.com>
2024-08-05 16:47:50 +0200

    modified

--- Models/Label.cs
+++ Models/Label.cs
@@ -4,8 +4,6 @@ namespace XmlToZpl.Models
 {
     public class Label
     {
-        // add verification par rapport NomLabel update status, dossier configModeleImpression qui conteint fichiers xml transferer et zpl avec le meme nom
-        // supression des fichiers l'ors de la supression d'un enregistrement
         public int Id { get; set; }
         public string NomLabel { get; set; }
         public string CheminLabel { get; set; }
@@ -13,7 +11,7 @@ namespace XmlToZpl.Models
         public int ModeleParDefaut { get; set; }
         public override string ToString()
         {
-            return $"Label: Id {Id}, {NomLabel}, Chemin: {CheminLabel}, ModeleParDefaut: {ModeleParDefaut}";
+            return $"Label: Id {Id}, {NomLabel}, CheminLabel: {CheminLabel}, CheminZpl: {CheminZpl}, ModeleParDefaut: {ModeleParDefaut}";
         }
     }
 }
